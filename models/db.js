ef90fcfb0b035309e26cdba0b2352547aefe03ea
John-Rocha <52468127+John-Rocha@users.noreply.github.com>
2021-01-28 23:51:32 -0300

    Change pass of db

--- models/db.js
+++ models/db.js
@@ -1,9 +1,10 @@
 const {Sequelize} = require('sequelize')
 
 //Conexão com o banco de dados MySql
-const sequelize = new Sequelize('postapp', 'root', '123456', {
+const sequelize = new Sequelize('postapp', 'root', '131087', {
     host: "localhost",
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 })
 
 module.exports = {
